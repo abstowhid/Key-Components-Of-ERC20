055b3c47af9d1b766e1b18b5966d85bc12baece8
Towhid <blatowhid@gmail.com>
2025-03-25 20:40:32 +0600

    Update 1_Zero_address.sol

--- 1_Zero_address.sol
+++ 1_Zero_address.sol
@@ -7,3 +7,10 @@ No one owns it.
 Used in Ethereum to represent something coming from nothing.
 
 Used in token contracts for minting and burning.
+
+===========================================================================================
+/////How does address(0) work?
+Operation	    from Address	      to  Address	        Effect
+Minting	     address(0)	        User Address	      Tokens are created from nothing and added to to
+Burning    	  User Address	    address(0)	        Tokens are removed from from and disappear
+Transfer	  User Address	      User Address	      Normal transfer, moving tokens from from to to
