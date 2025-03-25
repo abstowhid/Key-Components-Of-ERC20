e915b9d2ae9891597a7703b0eb8198ddbc0805e4
Towhid <blatowhid@gmail.com>
2025-03-25 20:37:37 +0600

    Update 1_Zero_address.sol

--- 1_Zero_address.sol
+++ 1_Zero_address.sol
@@ -1,1 +1,9 @@
+//0 addreess is special address in ethereum.
+=============================================================
+0x0000000000000000000000000000000000000000
+==========================================================
+No one owns it.
 
+Used in Ethereum to represent something coming from nothing.
+
+Used in token contracts for minting and burning.
